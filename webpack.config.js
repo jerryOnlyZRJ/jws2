d14464c04ff03472f011b4101819d787388fd9cc
ranjayzheng <ranjayzheng@tencent.com>
2018-09-10 15:05:04 +0800

    comment tinypng proxy

--- webpack.config.js
+++ webpack.config.js
@@ -171,8 +171,8 @@ let _localConfig = {
         }),
         new tinyPngWebpackPlugin({
             key:"KsylMrS0tgwrmpc93OCkW2CRwdtLTNal", //can be Array, eg:['your key 1','your key 2'....]
-            ext: ['png', 'jpeg', 'jpg'], //img ext name
-            proxy:'http://user:pass@192.168.0.1:8080' //http proxy,eg:如果你来自中国，同时拥有shadowsocks，翻墙默认配置为 http:127.0.0.1:1080 即可。（注，该参数因为需要超时断开连接的原因，导致最后会延迟执行一会webpack。但相对于国内网络环境，用此参数还是非常划算的，测试原有两张图片，无此参数耗时2000ms+，有此参数耗时1000ms+节约近半。）
+            ext: ['png', 'jpeg', 'jpg'],    //img ext name
+            // proxy:'http://user:pass@192.168.0.1:8080' //http proxy,eg:如果你来自中国，同时拥有shadowsocks，翻墙默认配置为 http:127.0.0.1:1080 即可。（注，该参数因为需要超时断开连接的原因，导致最后会延迟执行一会webpack。但相对于国内网络环境，用此参数还是非常划算的，测试原有两张图片，无此参数耗时2000ms+，有此参数耗时1000ms+节约近半。）
         }),
         ...spritesPlugins,
         new CleanWebpackPlugin(['dist/assets/*', 'dist/views/*'], {
