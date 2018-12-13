006ee76ae7b7e484e61a6e19582a4cc14dd4dace
ranjayzheng <ranjayzheng@qq.com>
2018-12-13 16:53:22 +0800

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     testMatch: ['<rootDir>/tests/*.spec.js'],
-    coverageDirectory: '<rootDir>/docs/coverage',
+    coverageDirectory: '<rootDir>/logs/coverage',
     coverageThreshold: { // 测试覆盖率, 阈值不满足，就返回测试失败
         global: {
             branches: 90,
